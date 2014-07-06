070780db1cb74feab237aaab469ee3a4e508043b
Stefan Gojan <vollekannehoschi@gmail.com>
2014-07-06 19:25:27 +0200

    add mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,52 @@
 'use strict';
 
-module.exports = function (grunt) {
-  // Show elapsed time at the end
-  require('time-grunt')(grunt);
-  // Load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
+    // Show elapsed time at the end
+    require('time-grunt')(grunt);
+    // Load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test']
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                src: ['lib/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js']
+            }
+        },
+        mochacov: {
+            options: {
+                reporter: 'spec'
+                //require: ['should']
+            },
+            all: '<%= jshint.test.src %>'
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib: {
+                files: '<%= jshint.lib.src %>',
+                tasks: ['jshint:lib', 'nodeunit']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test']
+            }
+        }
+    });
 
-  // Default task.
-  grunt.registerTask('default', ['jshint']);
+    // Default task.
+	grunt.registerTask('test', ['mochacov']);
+    grunt.registerTask('default', ['jshint', 'test']);
 };
